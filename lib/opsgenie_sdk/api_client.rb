a7d1c479ac62bf7821bc04d1a95358dfbc45ecc7
Quentin Rousseau <contact@quent.in>
2021-11-22 16:06:56 -0800

    ruby 3.0 compatible

--- lib/opsgenie_sdk/api_client.rb
+++ lib/opsgenie_sdk/api_client.rb
@@ -266,7 +266,7 @@ module OpsgenieSdk
     def build_request_url(path)
       # Add leading and trailing slashes to path
       path = "/#{path}".gsub(/\/+/, '/')
-      URI.encode(@config.base_url + path)
+      CGI::escape(@config.base_url + path)
     end
 
     # Builds the HTTP request body
